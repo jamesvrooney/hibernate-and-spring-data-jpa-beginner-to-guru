971f4921f19f51893e8960bb6e26693aacd8fb95
jamesvrooney <jamesvrooney@hotmail.com>
2021-12-06 16:15:59 +0000

    Added liquibase support.

---
+++ src/main/java/com/rooney/james/sdjpaintro/domain/Book.java
@@ -0,0 +1,24 @@
+package com.rooney.james.sdjpaintro.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    private String isbn;
+    private String publisher;
+}
