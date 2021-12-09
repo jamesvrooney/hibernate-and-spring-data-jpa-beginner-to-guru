b89c75fd6db8b404f332bdfbd66069db47c777ce
jamesvrooney <jamesvrooney@hotmail.com>
2021-12-09 21:38:13 +0000

    Save a new Author using entityManager.

--- src/main/java/com/rooney/james/sdjpaintro/domain/dao/impl/AuthorDaoImpl.java
+++ src/main/java/com/rooney/james/sdjpaintro/domain/dao/impl/AuthorDaoImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
 
 @RequiredArgsConstructor
 @Component
@@ -23,12 +24,30 @@ public class AuthorDaoImpl implements AuthorDAO {
 
     @Override
     public Author getByName(String firstName, String lastName) {
-        return null;
+        TypedQuery<Author> query = getEntityManager().createQuery("SELECT a FROM Author a "
+                + "WHERE a.firstName = :firstName "
+                + "AND a.lastName = :lastName", Author.class);
+
+        query.setParameter("firstName", firstName);
+        query.setParameter("lastName", lastName);
+
+        Author author = query.getSingleResult();
+
+        return author;
     }
 
     @Override
     public Author saveNewAuthor(Author newAuthor) {
-        return null;
+        EntityManager em = getEntityManager();
+
+        em.getTransaction().begin();
+
+        em.persist(newAuthor);
+        em.flush();
+
+        em.getTransaction().commit();
+
+        return newAuthor;
     }
 
     @Override
